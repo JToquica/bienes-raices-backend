4579cef7f768bb90b5583354cbb3975d6e3a634c
Jose Toquica <jose.herley.2010@gmail.com>
2022-07-08 16:08:00 -0500

    Inicio Proyecto

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+//Crear la app
+const app = express();
+const port = 3000;
+
+//Crear Routing
+app.get("/", (req, res) => {
+    res.send("Hola Mundo");
+})
+
+//Definir puerto de escucha
+app.listen(port, () => {
+    console.log(`Corriendo en el puerto ${port}`);
+})
